890275610d6e2d6f5119e8005d52839e456c2b95
AyatukiRin <zrl02081653@gmail.com>
2021-03-15 14:40:27 +0800

    P2006 想太多了，垃圾大法师！！！！ 他只判断技能能不能打死就行了。 注意变量类型的限制和特殊情况的出现。

--- Note/Test.py
+++ Note/Test.py
@@ -1,23 +1,34 @@
-lt = [1, 2, 3]
-index = lt.index(2)
-print(index)
+# lt = [1, 2, 3]
+# index = lt.index(2)
+# print(index)
+#
+#
+# for i in range(1, 2):
+#     print(i)
+#
+# def foo1():
+#     a = 1
+#     b = 2
+#     return a,b
+# a = [1,2]
+# for j in range(3):
+#     for i in a:
+#         print(i, end=' ')
+#     print('')
+#
+# if 6 in range(5,6):
+#     print('zai')
+#
+# dic = {'a': [1, 2]}
+# print(dic['a'][0])
 
 
-for i in range(1, 2):
-    print(i)
+# print(5 // 0)   # 地板除
 
-def foo1():
-    a = 1
-    b = 2
-    return a,b
-a = [1,2]
-for j in range(3):
-    for i in a:
-        print(i, end=' ')
-    print('')
 
-if 6 in range(5,6):
-    print('zai')
+a = {1: 3, 2: 2, 3: 1 }
+for value in a.values():
+    print(value)
 
-dic = {'a': [1, 2]}
-print(dic['a'][0])
+
+# print(5 % 3)
